fb35aaa40a11b8c99c7a6795d0b6cac3f177fa8f
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-10-07 15:20:11 +0530

    issue fixed

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
-const app = require('../app');
+let app;
+try {
+  app = require('../app');
+} catch (e) {
+  console.error('App import failed:', e);
+  module.exports = (_req, res) => res.status(500).json({ error: 'BOOT_FAIL', detail: String(e && e.stack || e) });
+  return;
+}
 module.exports = (req, res) => app(req, res);
