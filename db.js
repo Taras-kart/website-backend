0bc88efccce9913445e972bb55a86321c7a3d4d2
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-10-14 15:37:38 +0530

    new 2

--- db.js
+++ db.js
@@ -2,19 +2,26 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const CONNECTION_STRING =
+function withSslmodeRequire(cs) {
+  if (!cs) return '';
+  if (cs.includes('sslmode=')) return cs;
+  return cs.includes('?') ? `${cs}&sslmode=require` : `${cs}?sslmode=require`;
+}
+
+const CONNECTION_STRING = withSslmodeRequire(
   process.env.DATABASE_URL ||
   process.env.POSTGRES_URL ||
   process.env.POSTGRES_PRISMA_URL ||
   process.env.SUPABASE_DB_URL ||
-  '';
+  ''
+);
 
 if (!global._pgPool) {
   global._pgPool = new Pool({
     connectionString: CONNECTION_STRING,
     ssl: { require: true, rejectUnauthorized: false },
-    max: 3,
-    idleTimeoutMillis: 10000,
+    max: 5,
+    idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 10000
   });
 }
