af7f32b4fc4d9bc7163980c7061d6aa85d5d1e40
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-10-17 22:01:59 +0530

    hope 1

--- db.js
+++ db.js
@@ -1,29 +1,11 @@
-// D:\shopping-backend\db.js
-require('dotenv').config();
 const { Pool } = require('pg');
 
-function withSslmodeRequire(cs) {
-  if (!cs) return '';
-  if (cs.includes('sslmode=')) return cs;
-  return cs.includes('?') ? `${cs}&sslmode=require` : `${cs}?sslmode=require`;
-}
+const pool = new Pool({
+  connectionString: 'postgresql://h5pld5:xau_LRR5FBytMeO7OU8L8KgvFh5uvKkgd0tv0@eu-central-1.sql.xata.sh/taraskart:main?sslmode=require',
+  ssl: { require: true, rejectUnauthorized: false },
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000
+});
 
-const CONNECTION_STRING = withSslmodeRequire(
-  process.env.DATABASE_URL ||
-  process.env.POSTGRES_URL ||
-  process.env.POSTGRES_PRISMA_URL ||
-  process.env.SUPABASE_DB_URL ||
-  ''
-);
-
-if (!global._pgPool) {
-  global._pgPool = new Pool({
-    connectionString: CONNECTION_STRING,
-    ssl: { require: true, rejectUnauthorized: false },
-    max: 5,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 10000
-  });
-}
-
-module.exports = global._pgPool;
+module.exports = pool;
