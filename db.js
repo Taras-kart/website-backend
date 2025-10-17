661c0c4e542aaa58ca1c0eae15178e826e0278ab
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-10-17 22:55:38 +0530

    db password updated

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://h5pld5:xau_LRR5FBytMeO7OU8L8KgvFh5uvKkgd0tv0@eu-central-1.sql.xata.sh/taraskart:main?sslmode=require',
+  connectionString: 'postgresql://h5pld5:xau_ijXYyK3RMaZ91wJ64ig5qUmauAtEp3J61@eu-central-1.sql.xata.sh:5432/taraskart:main?sslmode=require',
   ssl: { require: true, rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
