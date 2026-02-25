84257c7a40e5f7e278510dedfa176074d2e3ea0f
HarshaMW1 <142015096+HarshaMW1@users.noreply.github.com>
2026-02-25 12:02:30 +0530

    changed from xata to neon

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://h5pld5:xau_ijXYyK3RMaZ91wJ64ig5qUmauAtEp3J61@eu-central-1.sql.xata.sh:5432/taraskart:main?sslmode=require',
+  connectionString: process.env.DATABASE_URL,
   ssl: { require: true, rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
