cf3b5d513047189c4195697010dcfe059be66d85
HarshaMW1 <142015096+HarshaMW1@users.noreply.github.com>
2026-02-27 12:01:48 +0530

    changed to prod db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_L4WCoHgsFbt1@ep-lingering-lab-a1plroab-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  connectionString: 'postgresql://neondb_owner:npg_PVJ3NUMx0ydu@ep-plain-union-a15hwhla-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
   ssl: { require: true, rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
