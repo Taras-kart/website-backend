b19f12ddeef3f0fd3612b184f6c45f7d0ba3e730
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2026-03-19 12:30:03 +0530

    url changed

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_PVJ3NUMx0ydu@ep-plain-union-a15hwhla-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
-  ssl: { require: true, rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000
